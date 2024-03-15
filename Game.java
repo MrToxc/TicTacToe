e671f3805131f3d0f7a3f55cf1c9690c2f8e1d2e
tomik <tomik.hasek@gmail.com>
2024-03-15 13:35:59 +0100

    annoying stuff done

--- Game.java
+++ Game.java
@@ -21,6 +21,15 @@ public class Game {
         for (int i = 0; i < 9; i++) {
             arr.add(new Square());
         }
+        arr.get(0).setLocation(Location.A1);
+        arr.get(1).setLocation(Location.A2);
+        arr.get(2).setLocation(Location.A3);
+        arr.get(3).setLocation(Location.B1);
+        arr.get(4).setLocation(Location.B2);
+        arr.get(5).setLocation(Location.B3);
+        arr.get(6).setLocation(Location.C1);
+        arr.get(7).setLocation(Location.C2);
+        arr.get(8).setLocation(Location.C3);
     }
 
     public void addX() {
--- Square.java
+++ Square.java
@@ -1,4 +1,20 @@
 public class Square {
     TypesOfSquare types;
     Location location;
+
+    public TypesOfSquare getTypes() {
+        return types;
+    }
+
+    public void setTypes(TypesOfSquare types) {
+        this.types = types;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
 }
