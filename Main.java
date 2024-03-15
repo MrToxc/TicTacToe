5f3dc6bcff07c981ec7c22e34fd7391b2717eb74
tomik <tomik.hasek@gmail.com>
2024-03-15 13:06:25 +0100

    update of classes and methods

--- Main.java
+++ Main.java
@@ -1,6 +1,10 @@
 class Main {
     public static void main(String[] args) {
 
+    UserOutputConsole UserOutput= new UserOutputConsole();
+
+    UserOutput.println("Vitejte v naší hře");
+
 
     }
 }
